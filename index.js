c99d261951d59c7a7cd7c558bbf48c723adf8e66
Sam Finan <samcfinan@gmail.com>
2019-03-15 14:46:53 -0400

    Split into modules, working on polling func

--- index.js
+++ index.js
@@ -1,94 +1,20 @@
-const pubsub = require('@google-cloud/pubsub');
-
-const client = new pubsub.v1.SubscriberClient();
-// process.env.GOOGLE_APPLICATION_CREDENTIALS="./transcode-cloud-service-account.json"
-
-const projectName = process.env.PROJECT_NAME;
-const subscriptionName = process.env.SUBSCRIPTION;
-
-const formattedSubscription = client.subscriptionPath(
-  projectName,
-  subscriptionName
-);
-
-// The maximum number of messages returned for this request.
-// Pub/Sub may return fewer than the number specified.
-const maxMessages = 1;
-const newAckDeadlineSeconds = 10;
-const request = {
-  subscription: formattedSubscription,
-  maxMessages: maxMessages,
-};
-
-// Simulated worker function:
-// The worker function is meant to be non-blocking. It starts a long-
-// running process, such as writing the message to a table, which may
-// take longer than the default 10-sec acknowledge deadline.
-let isProcessed = false;
-
-const worker = async (message) => {
-  console.log(`Processing "${message.message.data}"...`);
-  
-  return new Promise(w => setTimeout(w => {
-    console.log(`Finished processing "${message.message.data}".`);
-    isProcessed = true;
-  }, 5000));
-}
-
-const listen = async () => {
-  
-  const [response] = await client.pull(request);
-
-  // The subscriber pulls a specified number of messages.
-  // Obtain the first message.
-  const message = response.receivedMessages[0];
-  console.log(message);
-  // Send the message to the worker function.
-  await worker(message);
-  
-  let waiting = true;
-  while (waiting) {
-    await new Promise(r => setTimeout(r, 5000));
-    // If the message has been processed..
-    if (isProcessed) {
-      const ackRequest = {
-        subscription: formattedSubscription,
-        ackIds: [message.ackId],
-      };
-  
-      //..acknowledges the message.
-      console.log(`Acknowledged: "${message.message.data}".`);
-      // Exit after the message is acknowledged.
-      waiting = false;
-      console.log(`Done.`);
-      return await client.acknowledge(ackRequest);
-    } else {
-      // If the message is not yet processed..
-      const modifyAckRequest = {
-        subscription: formattedSubscription,
-        ackIds: [message.ackId],
-        ackDeadlineSeconds: newAckDeadlineSeconds,
-      };
-  
-      //..reset its ack deadline.
-      await client.modifyAckDeadline(modifyAckRequest);
-  
-      console.log(
-        `Reset ack deadline for "${
-          message.message.data
-        }" for ${newAckDeadlineSeconds}s.`
-      );
-    }
-  }
-}
+const config = require("./config/config.json");
 
 // TODO
 // Set up polling interval if not actively working
 
-// const main = async () => {
-//   setInterval(() => {
+class Server {
+  constructor() {
+    this.active = false;
+  }
 
-//   }, 5000)
-// }
+  async poll() {
+  }
+}
 
-// main();
+outOfMessages = false;
+const main = async () => {
+  const server = new Server();
+}
+
+main();
--- start.js
+++
@@ -1,7 +0,0 @@
-const forever = require('forever-monitor')
-
-const child = new (forever.Monitor)('index.js', {
-  max: 1,
-  silent: false
-})
-child.start()
