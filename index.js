53aadcbf0a66afd68430980cff9eaf12bfd30a8d
Sam Finan <samcfinan@gmail.com>
2019-03-24 14:34:56 -0400

    Worker working properly!

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 const config = require("./config/config.json");
+const { pollMessages } = require("./lib/pubsub");
 
 // TODO
 // Set up polling interval if not actively working
 
 class Server {
   constructor() {
-    this.active = false;
+    this.working = false;
+    this.worker = null;
+    this.interval = setInterval(() => {
+      if (!this.working) {
+        this.poll();
+      }
+    }, 5000);
+    
   }
 
   async poll() {
+    const poll = await pollMessages()
+    if (poll.working) {
+      this.working = true;
+      await this.work(poll.job);
+    }
+  }
+
+  // Fake work job
+  async work(job) {
+    this.working = true;
+    console.log("Working");
+    return new Promise(() => setTimeout(() => {
+      console.log("Work complete");
+      this.working = false;
+    }, 5000))
   }
 }
 
 outOfMessages = false;
 const main = async () => {
   const server = new Server();
-}
+};
 
 main();
